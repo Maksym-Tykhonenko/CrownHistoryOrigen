35da6e0111422e53e2b028648d20a0a43165ecb1
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-01-29 19:06:23 +0200

    sss

--- App.jsx
+++ App.jsx
@@ -482,7 +482,7 @@ const App = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log(checkUrl);
 
-    const targetData = new Date('2025-01-21T10:00:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-02-01T10:00:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (!route) {
